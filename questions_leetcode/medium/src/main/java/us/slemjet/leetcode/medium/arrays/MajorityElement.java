76e989fc493275187326d015ae7af8f654ad68c1
SLem <slemjet@gmail.com>
2020-07-07 20:37:12 -0700

    leetcode - 169. Majority Element

---
+++ questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/arrays/MajorityElement.java
@@ -0,0 +1,62 @@
+package us.slemjet.leetcode.medium.arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+
+    /**
+     * Runtime:  99.84% - O(n)
+     * Memory Usage: 55.57% - O(1)
+     */
+    public int majorityElement(int[] nums) {
+        int major = nums[0];
+        int count = 1;
+        int threshold = nums.length / 2;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (major == nums[i]) {
+                count++;
+
+                if (count > threshold) {
+                    return major;
+                }
+            } else {
+                count--;
+
+                if (count == 0) {
+                    major = nums[i];
+                    count = 1;
+                }
+            }
+        }
+
+        return major;
+    }
+
+    /**
+     * Runtime:  9.95% - O(n)
+     * Memory Usage: 5.05% - O(n)
+     */
+    public int majorityElementHashTable(int[] nums) {
+
+        Map<Integer, Integer> occurrences = new HashMap<>();
+
+        int threshold = nums.length / 2;
+
+        for (int num : nums) {
+            Integer count = occurrences.get(num);
+            if (count == null)
+                count = 0;
+
+            count++;
+
+            occurrences.put(num, count);
+
+            if (count > threshold)
+                return num;
+        }
+
+        return 0;
+    }
+}
---
+++ questions_leetcode/medium/src/test/java/us/slemjet/leetcode/medium/arrays/MajorityElementTest.java
@@ -0,0 +1,44 @@
+package us.slemjet.leetcode.medium.arrays;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class MajorityElementTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(new int[]{3, 2, 3}, 3),
+                Arguments.of(new int[]{2, 2, 1, 1, 1, 2, 2}, 2)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testSolution(int[] input, int expected) {
+        // given
+        MajorityElement solution = new MajorityElement();
+
+        // when
+        int value = solution.majorityElement(input);
+
+        // then
+        Assertions.assertThat(value).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testSolution2(int[] input, int expected) {
+        // given
+        MajorityElement solution = new MajorityElement();
+
+        // when
+        int value = solution.majorityElementHashTable(input);
+
+        // then
+        Assertions.assertThat(value).isEqualTo(expected);
+    }
+}
