36a6269f6bba7acde89c0385968a4512d6ca88cc
SLem <slemjet@gmail.com>
2020-08-10 21:43:03 -0700

    leetcode - 567. Permutation in String

---
+++ questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/strings/PermutationInString.java
@@ -0,0 +1,62 @@
+package us.slemjet.leetcode.medium.strings;
+
+public class PermutationInString {
+
+    /**
+     * Use sliding window
+     * Runtime: 4 ms, faster than 88.59%
+     * Memory Usage: 39.4 MB, less than 87.42%
+     */
+    public boolean checkInclusion(String s1, String s2) {
+
+        int[] ref = new int[26];
+
+        int refMatch = 0;
+        for (char refChar : s1.toCharArray()) {
+            int refIdx = refChar - 'a';
+            ref[refIdx]++;
+            if (ref[refIdx] == 1)
+                refMatch++;
+        }
+
+        int left = 0;
+        int right = 0;
+        int[] curr = new int[26];
+        int currMatch = 0;
+
+        char[] chars = s2.toCharArray();
+
+        while (right < chars.length) {
+
+            // right
+            int rIdx = chars[right] - 'a';
+            curr[rIdx]++;
+
+            if (curr[rIdx] == ref[rIdx])
+                currMatch++;
+
+            if (curr[rIdx] == ref[rIdx] + 1)
+                currMatch--;
+
+            right++;
+
+            if (right > s1.length()) {
+                // left
+                int lIdx = chars[left] - 'a';
+                curr[lIdx]--;
+
+                if (curr[lIdx] == ref[lIdx])
+                    currMatch++;
+
+                if (curr[lIdx] == ref[lIdx] - 1)
+                    currMatch--;
+
+                left++;
+            }
+
+            if (currMatch == refMatch)
+                return true;
+        }
+        return false;
+    }
+}
---
+++ questions_leetcode/medium/src/test/java/us/slemjet/leetcode/medium/strings/PermutationInStringTest.java
@@ -0,0 +1,34 @@
+package us.slemjet.leetcode.medium.strings;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.of;
+
+class PermutationInStringTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                of("ab", "eidbaooo", true),
+                of("ab", "eidboaoo", false),
+                of("adc", "dcda", true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void checkInclusion(String s1, String s2, boolean expected) {
+        // given
+        PermutationInString solution = new PermutationInString();
+
+        // when
+        boolean result = solution.checkInclusion(s1, s2);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
