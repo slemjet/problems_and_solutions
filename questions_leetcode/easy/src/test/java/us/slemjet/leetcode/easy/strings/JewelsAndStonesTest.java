26408caf0837bf1e57d5321120d29e1b61561e68
SLem <slemjet@gmail.com>
2020-08-15 14:46:17 -0700

    leetcode - 771. Jewels and Stones

---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/strings/JewelsAndStonesTest.java
@@ -0,0 +1,31 @@
+package us.slemjet.leetcode.easy.strings;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class JewelsAndStonesTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("aA", "aAAbbbb", 3),
+                Arguments.of("z", "ZZ", 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void reverseTest(String J, String S, int expected) {
+        // given
+        JewelsAndStones solution = new JewelsAndStones();
+
+        // when
+        int result = solution.numJewelsInStones(J, S);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
