8d2b917f9ab096b1f4cea6c1a4a967d89b763f90
SLem <slemjet@gmail.com>
2020-08-22 13:44:10 -0700

    leetcode - 986. Interval List Intersections

---
+++ questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/arrays/IntervalListIntersections.java
@@ -0,0 +1,35 @@
+package us.slemjet.leetcode.medium.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntervalListIntersections {
+
+    /**
+     * Runtime: 7 ms, faster than 21.07%
+     * Memory Usage: 52.4 MB, less than 29.27%
+     */
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+
+        List<int[]> result = new ArrayList<>();
+
+        int aIdx = 0;
+        int bIdx = 0;
+
+        while (aIdx < A.length && bIdx < B.length) {
+
+            int start = Math.max(A[aIdx][0], B[bIdx][0]); // get possible start of interval
+            int end = Math.min(A[aIdx][1], B[bIdx][1]); // get possible end of interval
+
+            if (start <= end)
+                result.add(new int[]{start, end}); // pair is correct - add it
+
+            if (A[aIdx][1] < B[bIdx][1])
+                aIdx++; // A pair is less - skip to next A pair
+            else
+                bIdx++; // B pair is less - skip to next B pir
+        }
+
+        return result.toArray(new int[0][]);
+    }
+}
---
+++ questions_leetcode/medium/src/test/java/us/slemjet/leetcode/medium/arrays/IntervalListIntersectionsTest.java
@@ -0,0 +1,50 @@
+package us.slemjet.leetcode.medium.arrays;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class IntervalListIntersectionsTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(
+                        new int[][]{
+                                new int[]{0, 2},
+                                new int[]{5, 10},
+                                new int[]{13, 23},
+                                new int[]{24, 25}
+                        }, new int[][]{
+                                new int[]{1, 5},
+                                new int[]{8, 12},
+                                new int[]{15, 24},
+                                new int[]{25, 26}
+                        }, new int[][]{
+                                new int[]{1, 2},
+                                new int[]{5, 5},
+                                new int[]{8, 10},
+                                new int[]{15, 23},
+                                new int[]{24, 24},
+                                new int[]{25, 25}
+                        }
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void intervalIntersection(int[][] A, int[][] B, int[][] expected) {
+        // given
+        IntervalListIntersections solution = new IntervalListIntersections();
+
+        // when
+        int[][] result = solution.intervalIntersection(A, B);
+
+        // then
+        Assertions.assertThat(result).containsExactlyInAnyOrder(expected);
+    }
+
+}
