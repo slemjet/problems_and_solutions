53e2c5f75795a8f16ce2b4716044edd9d08498cc
SLem <slemjet@gmail.com>
2020-09-06 10:37:42 -0700

    leetcode - 1035. Uncrossed Lines

--- questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/arrays/UncrossedLines.java
+++ questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/arrays/UncrossedLines.java
@@ -3,6 +3,25 @@ package us.slemjet.leetcode.medium.arrays;
 public class UncrossedLines {
 
     /**
+     * Runtime: 96.49%
+     * Memory Usage: 90.93%
+     */
+    public int maxUncrossedLinesDpCycle(int[] A, int[] B) {
+        int[][] dp = new int[A.length + 1][B.length + 1];
+
+        for (int i = 1; i <= A.length; i++) {
+            for (int j = 1; j <= B.length; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[A.length][B.length];
+    }
+
+    /**
      * Runtime: 6.4%
      * Memory Usage: 7.2%
      */
@@ -32,26 +51,6 @@ public class UncrossedLines {
         dp[i][j] = result;
         return result;
     }
-
-    /**
-     * Runtime: 96.49%
-     * Memory Usage: 90.93%
-     */
-    public int maxUncrossedLinesDpCycle(int[] A, int[] B) {
-        int[][] dp = new int[A.length + 1][B.length + 1];
-
-        for (int i = 1; i <= A.length; i++) {
-            for (int j = 1; j <= B.length; j++) {
-                if (A[i - 1] == B[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-                }
-            }
-        }
-        return dp[A.length][B.length];
-    }
-
 
     public int maxUncrossedLinesSimple(int[] A, int[] B) {
         return calculateCrossed(A, B, 0, 0);
