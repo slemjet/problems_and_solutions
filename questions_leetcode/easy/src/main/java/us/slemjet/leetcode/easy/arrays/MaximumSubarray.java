cf1169979ee796ad47b243547fb9a91b74667293
SLem <slemjet@gmail.com>
2020-09-12 19:36:28 -0700

    leetcode - 53. Maximum Subarray

---
+++ questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/arrays/MaximumSubarray.java
@@ -0,0 +1,38 @@
+package us.slemjet.leetcode.easy.arrays;
+
+
+public class MaximumSubarray {
+
+    /**
+     * Runtime: 74.15%
+     * Memory Usage: 5.00%
+     */
+    public int maxSubArray(int[] nums) {
+
+        int result = Integer.MIN_VALUE;
+        int currSum = 0;
+
+        for (int num : nums) {
+            currSum = Math.max(currSum + num, num);
+            result = Math.max(currSum, result);
+        }
+
+        return result;
+    }
+
+    /**
+     * Runtime: 74.15%
+     * Memory Usage: 24.01%
+     */
+    public int maxSubArray2(int[] nums) {
+
+        int result = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] =  Math.max(nums[i - 1] + nums[i], nums[i]);
+            result = Math.max(nums[i], result);
+        }
+
+        return result;
+    }
+}
