2c89df39bb268c8ddc578f0abd69da90e7e3c8f3
SLem <slemjet@gmail.com>
2020-09-19 16:45:38 -0700

    leetcode - 66. Plus One

---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/numbers/PlusOneTest.java
@@ -0,0 +1,33 @@
+package us.slemjet.leetcode.easy.numbers;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class PlusOneTest {
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3}, new int[]{1, 2, 4}),
+                Arguments.of(new int[]{4, 3, 2, 1}, new int[]{4, 3, 2, 2}),
+                Arguments.of(new int[]{0}, new int[]{1}),
+                Arguments.of(new int[]{9}, new int[]{1, 0})
+
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void plusOne(int[] digits, int[] expected) {
+        // given
+        PlusOne solution = new PlusOne();
+
+        // when
+        int[] result = solution.plusOne(digits);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
