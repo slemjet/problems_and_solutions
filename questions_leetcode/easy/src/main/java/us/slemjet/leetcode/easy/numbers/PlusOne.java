83dc7415c0ff04b58be36cd4a8b692d04fbfb6a5
SLem <slemjet@gmail.com>
2020-09-19 16:45:11 -0700

    leetcode - 62. Unique Paths

---
+++ questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/numbers/PlusOne.java
@@ -0,0 +1,28 @@
+package us.slemjet.leetcode.easy.numbers;
+
+public class PlusOne {
+
+    /**
+     * Runtime: 100.00%
+     * Memory Usage: 54.16%
+     */
+    public int[] plusOne(int[] digits) {
+
+        int idx = digits.length - 1;
+        digits[idx]++;
+
+        while (digits[idx] >= 10) {
+
+            digits[idx] -= 10;
+            if (idx == 0) {
+                int[] extendedDigits = new int[digits.length + 1];
+                System.arraycopy(digits, 0, extendedDigits, 1, digits.length - 1 + 1);
+                digits = extendedDigits;
+                idx++;
+            }
+            digits[--idx]++;
+        }
+
+        return digits;
+    }
+}
