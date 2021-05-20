a45a411dc829dcd301a3cdd411f7fa3bf8540061
slemjet <slemjet@gmail.com>
2021-05-19 21:05:21 -0700

    130. Surrounded Regions

--- questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/from_100_to_150/SurroundedRegions.java
+++ questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/from_100_to_150/SurroundedRegions.java
@@ -6,50 +6,43 @@ package us.slemjet.leetcode.medium.from_100_to_150;
 public class SurroundedRegions {
 
     /**
-     * Runtime: 99.55%
-     * Memory Usage: 51.81%
+     * Runtime: 99.56%
+     * Memory Usage: 63.66%
      */
     public void solve(char[][] board) {
 
         if (board == null) return;
 
-        boolean[][] bordered = new boolean[board.length][board[0].length];
-
         // mark all bordered cells. rest can be switched.
-
         for (int i = 0; i < board[0].length; i++) {
-            checkNeighbours(board, bordered, 0, i);
-        }
-
-        for (int i = 0; i < board[0].length; i++) {
-            checkNeighbours(board, bordered, board.length - 1, i);
+            checkNeighbours(board, 0, i);
+            checkNeighbours(board, board.length - 1, i);
         }
 
         for (int i = 0; i < board.length; i++) {
-            checkNeighbours(board, bordered, i, 0);
-        }
-
-        for (int i = 0; i < board.length; i++) {
-            checkNeighbours(board, bordered, i, board[0].length - 1);
+            checkNeighbours(board, i, 0);
+            checkNeighbours(board, i, board[0].length - 1);
         }
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == 'O' && !bordered[i][j]) {
+                char c = board[i][j];
+                if (c == 'O') {
                     board[i][j] = 'X';
+                } else if (c == 'B') {
+                    board[i][j] = 'O';
                 }
             }
         }
     }
 
-    private void checkNeighbours(char[][] board, boolean[][] bordered, int x, int y) {
-        if (x >= 0 && x < board.length && y >= 0 && y < board[0].length && !bordered[x][y] && board[x][y] == 'O') {
-//            System.out.println(String.format("Bordered cell: [%s][%s]", x, y));
-            bordered[x][y] = true;
-            checkNeighbours(board, bordered, x - 1, y);
-            checkNeighbours(board, bordered, x + 1, y);
-            checkNeighbours(board, bordered, x, y - 1);
-            checkNeighbours(board, bordered, x, y + 1);
+    private void checkNeighbours(char[][] board, int x, int y) {
+        if (x >= 0 && x < board.length && y >= 0 && y < board[0].length && board[x][y] != 'B' && board[x][y] == 'O') {
+            board[x][y] = 'B';
+            checkNeighbours(board, x - 1, y);
+            checkNeighbours(board, x + 1, y);
+            checkNeighbours(board, x, y - 1);
+            checkNeighbours(board, x, y + 1);
         }
     }
 }
