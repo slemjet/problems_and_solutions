7022ecb06250856102f56b4d80da47ae5f6d89cc
slemjet <slemjet@gmail.com>
2021-06-22 00:23:50 -0700

    138. Copy List with Random Pointer

---
+++ questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/from_100_to_150/CopyListWithRandomPointer.java
@@ -0,0 +1,62 @@
+package us.slemjet.leetcode.medium.from_100_to_150;
+
+/**
+ * 138. Copy List with Random Pointer
+ */
+public class CopyListWithRandomPointer {
+
+    /**
+     * Runtime: 100.00%
+     * Memory Usage: 84.78%
+     */
+    public Node copyRandomList(Node head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        Node originalHead = head;
+        // Add copy node after each node
+        while (head != null) {
+            Node copy = new Node(head.val);
+            copy.next = head.next;
+            head.next = copy;
+            head = copy.next;
+        }
+
+        head = originalHead;
+        // Update random reference to copy
+        while (head != null) {
+            if (head.random != null) {
+                head.next.random = head.random.next;
+            }
+            head = head.next.next;
+        }
+
+        Node copyHead = originalHead.next;
+        head = originalHead;
+        // Restore original list and extract copy
+        while (head != null) {
+            Node copyNext = head.next;
+            Node headNext = head.next.next;
+            head.next = headNext;
+            if (headNext != null)
+                copyNext.next = headNext.next;
+            head = headNext;
+        }
+
+        return copyHead;
+    }
+
+    public static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+}
