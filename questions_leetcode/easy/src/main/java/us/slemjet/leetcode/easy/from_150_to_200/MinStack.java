181057b8638b6b665514afbb5fe8468ce3cee87c
slemjet <slemjet@gmail.com>
2021-10-18 21:53:53 -0700

    155. Min Stack

---
+++ questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/from_150_to_200/MinStack.java
@@ -0,0 +1,57 @@
+package us.slemjet.leetcode.easy.from_150_to_200;
+
+/**
+ * 155. Min Stack
+ *
+ * Runtime: 32.83%
+ * Memory Usage:37.60%
+ */
+class MinStack {
+
+    private Node head;
+
+    public MinStack() {
+    }
+
+    public void push(int val) {
+        if (head == null) {
+            head = new Node(val, val, null);
+        } else {
+            // Use previous node min value to calculate new node min value -> we don't need to track node with the actual min value
+            head = new Node(val, Math.min(val, head.min), head);
+        }
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.value;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    private class Node {
+        int value;
+        int min;
+        Node next;
+
+        public Node(int value, int min, Node next) {
+            this.value = value;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/from_150_to_200/MinStackTest.java
@@ -0,0 +1,24 @@
+package us.slemjet.leetcode.easy.from_150_to_200;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MinStackTest {
+
+    @Test
+    void testMinStack() {
+        // given
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        // when
+        int min1 = minStack.getMin();// return -3
+        minStack.pop();
+        int top = minStack.top();// return 0
+
+        // then
+        Assertions.assertThat(min1).isEqualTo(-3);
+        Assertions.assertThat(top).isEqualTo(0);
+    }
+}
