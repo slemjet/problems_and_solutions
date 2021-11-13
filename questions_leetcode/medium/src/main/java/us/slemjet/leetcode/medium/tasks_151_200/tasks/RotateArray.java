73a72dba0d7244e8a9d7ded6834342875f3b6c6e
slemjet <slemjet@gmail.com>
2021-11-13 10:58:13 -0800

    189. Rotate Array

---
+++ questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/tasks_151_200/tasks/RotateArray.java
@@ -0,0 +1,53 @@
+package us.slemjet.leetcode.medium.tasks_151_200.tasks;
+
+/**
+ * 189. Rotate Array
+ */
+public class RotateArray {
+
+    /**
+     * Using 3 rotates
+     * Runtime: 100.00%
+     * Memory Usage: 60.27%
+     */
+    public void rotate(int[] nums, int k) {
+
+        // Remove extra length
+        k = k % nums.length;
+
+        // [>,>,>,>,>>,>,>,>]
+        // [<,<,<,<<,>,>,>,>]
+        // [>,>,>,>>,>,>,>,>]
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end){
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+
+    /**
+     * Using next index formula and separate array
+     * Runtime: 67.66%
+     * Memory Usage: 70.09%
+     */
+    public void rotateSeparate(int[] nums, int k) {
+        int l = nums.length;
+        int[] rotated = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int nextIdx = (i + k) % l;
+            rotated[nextIdx] = nums[i];
+        }
+
+        System.arraycopy(rotated, 0, nums, 0, rotated.length);
+    }
+}
---
+++ questions_leetcode/medium/src/test/java/us/slemjet/leetcode/medium/tasks_151_200/RotateArrayTest.java
@@ -0,0 +1,46 @@
+package us.slemjet.leetcode.medium.tasks_151_200;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import us.slemjet.leetcode.medium.tasks_151_200.tasks.RotateArray;
+
+import java.util.stream.Stream;
+
+class RotateArrayTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 4, 5, 6, 7}, 3, new int[]{5, 6, 7, 1, 2, 3, 4}),
+                Arguments.of(new int[]{-1, -100, 3, 99}, 2, new int[]{3, 99, -1, -100}),
+                Arguments.of(new int[]{1, 2, 3}, 0, new int[]{1, 2, 3})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testsRotate(int[] nums, int k, int[] expected) {
+        // given
+        RotateArray solution = new RotateArray();
+
+        // when
+        solution.rotate(nums, k);
+
+        // then
+        Assertions.assertThat(nums).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testsRotateSeparate(int[] nums, int k, int[] expected) {
+        // given
+        RotateArray solution = new RotateArray();
+
+        // when
+        solution.rotateSeparate(nums, k);
+
+        // then
+        Assertions.assertThat(nums).isEqualTo(expected);
+    }
+}
