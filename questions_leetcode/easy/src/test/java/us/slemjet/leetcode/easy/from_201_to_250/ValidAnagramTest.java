b1c8cdefe01eb48f0674b9cf578d63aae57d4658
slemjet <slemjet@gmail.com>
2021-12-11 10:47:26 -0800

    242. Valid Anagram

---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/from_201_to_250/ValidAnagramTest.java
@@ -0,0 +1,31 @@
+package us.slemjet.leetcode.easy.from_201_to_250;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class ValidAnagramTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("anagram", "nagaram", true),
+                Arguments.of("rat", "car", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testIsAnagram(String s, String t, boolean expected) {
+        // given
+        ValidAnagram solution = new ValidAnagram();
+
+        // when
+        boolean result = solution.isAnagram(s, t);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
