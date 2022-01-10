0904bf5988d40a4ed797df6a7b3ab207a61902ef
slemjet <slemjet@gmail.com>
2022-01-09 16:17:05 -0800

    1137. N-th Tribonacci Number

---
+++ questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/from_1101_to_1150/tasks/NthTribonacciNumber.java
@@ -0,0 +1,77 @@
+package us.slemjet.leetcode.easy.from_1101_to_1150.tasks;
+
+/**
+ * 1137. N-th Tribonacci Number
+ */
+public class NthTribonacciNumber {
+
+    /**
+     * Iterative
+     * <p>
+     * Runtime: 100.00%
+     * Memory Usage: 72.67%
+     */
+    public int tribonacci(int n) {
+
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+
+        int a = 0;
+        int b = 1;
+        int c = 1;
+
+        while (n >= 3) {
+            int sum = a + b + c;
+            a = b;
+            b = c;
+            c = sum;
+            n--;
+        }
+
+        return c;
+    }
+
+    /**
+     * Top Down Approach
+     *
+     * Runtime: 100.00%
+     * Memory Usage: 27.53%
+     */
+    public int tribonacciMemo(int n) {
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+
+        return tribonacciMemo(new int[n + 1], n);
+    }
+
+    private int tribonacciMemo(int[] memo, int n) {
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+
+        if (memo[n] != 0) return memo[n];
+        memo[n] = tribonacciMemo(memo, n - 3) + tribonacciMemo(memo, n - 2) + tribonacciMemo(memo, n - 1);
+        return memo[n];
+    }
+
+    /**
+     * Bottom Up Approach
+     * <p>
+     * Runtime: 100.00%
+     * Memory Usage: 12.68%
+     */
+    public int tribonacciBottomUp(int n) {
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+
+        int[] result = new int[n + 1];
+        result[0] = 0;
+        result[1] = 1;
+        result[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            result[i] = result[i - 3] + result[i - 2] + result[i - 1];
+        }
+
+        return result[n];
+    }
+}
---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/from_1101_to_1150/tasks/NthTribonacciNumberTest.java
@@ -0,0 +1,58 @@
+package us.slemjet.leetcode.easy.from_1101_to_1150.tasks;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class NthTribonacciNumberTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(4, 4),
+                Arguments.of(25, 1389537),
+                Arguments.of(34, 334745777)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testTribonacci(int n, int expected) {
+        // given
+        NthTribonacciNumber solution = new NthTribonacciNumber();
+
+        // when
+        int result = solution.tribonacci(n);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testTribonacciMemo(int n, int expected) {
+        // given
+        NthTribonacciNumber solution = new NthTribonacciNumber();
+
+        // when
+        int result = solution.tribonacciMemo(n);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testTribonacciBottomUp(int n, int expected) {
+        // given
+        NthTribonacciNumber solution = new NthTribonacciNumber();
+
+        // when
+        int result = solution.tribonacciBottomUp(n);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
