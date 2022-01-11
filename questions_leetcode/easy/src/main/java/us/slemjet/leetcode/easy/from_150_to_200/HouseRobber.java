1db763cd9f138ffe24bc12f6e49bdcf645e1fc67
slemjet <slemjet@gmail.com>
2022-01-10 18:51:02 -0800

    198. House Robber

--- questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/from_150_to_200/HouseRobber.java
+++ questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/from_150_to_200/HouseRobber.java
@@ -1,24 +1,65 @@
 package us.slemjet.leetcode.easy.from_150_to_200;
 
+/**
+ * 198. House Robber
+ */
 public class HouseRobber {
 
+    /**
+     * Iterative approach
+     *
+     * Runtime: 100.00%
+     * Memory Usage: 15.35%
+     */
     public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
 
-        if (nums.length == 0)
-            return 0;
-        if (nums.length == 1)
-            return nums[0];
-        if (nums.length == 2)
-            return Math.max(nums[0], nums[1]);
+        int a = 0; // Base scenario is a = 0 b = nums[0]
+        int b = nums[0];
 
-        int[] memo = new int[nums.length];
-        memo[0] = nums[0];
-        memo[1] = Math.max(nums[0], nums[1]);
+        for (int i = 1; i < nums.length; i++) {
+            int current = Math.max(nums[i] + a, b);
+            a = b;
+            b = current;
+        }
+        return b;
+    }
 
-        for (int i = 2; i < nums.length ; i++) {
-            memo[i] = Math.max(nums[i] + memo[i - 2], memo[i - 1]);
+    /**
+     * Bottom Up
+     * <p>
+     * Runtime: 100.00%
+     * Memory Usage: 19.31%
+     */
+    public int robBottomUp(int[] nums) {
+
+        if (nums.length == 1) return nums[0];
+        // We can do it inline using nums as dp num[0] and nums[1] are base scenarios
+        nums[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i - 1], nums[i] + nums[i - 2]); // Can take either previous or current + [i - 2]
         }
 
-        return memo[memo.length - 1];
+        return nums[nums.length - 1];
+    }
+
+    /**
+     * Top To Bottom
+     * <p>
+     * Runtime: 100.00%
+     * Memory Usage: 15.35%
+     */
+    public int robTopToBottom(int[] nums) {
+        Integer[] memo = new Integer[nums.length];
+        return Math.max(robTopToBottom(nums, memo, nums.length - 2), nums[nums.length - 1] + robTopToBottom(nums, memo, nums.length - 3));
+    }
+
+    private int robTopToBottom(int[] nums, Integer[] memo, int i) {
+        if (i < 0) return 0;
+        if (memo[i] == null) {
+            memo[i] = Math.max(robTopToBottom(nums, memo, i - 1), nums[i] + robTopToBottom(nums, memo, i - 2));
+        }
+        return memo[i];
     }
 }
--- questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/from_150_to_200/HouseRobberTest.java
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/from_150_to_200/HouseRobberTest.java
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import us.slemjet.leetcode.easy.from_150_to_200.HouseRobber;
 
 import java.util.stream.Stream;
 
@@ -12,8 +11,11 @@ class HouseRobberTest {
 
     private static Stream<Arguments> parameters() {
         return Stream.of(
+                Arguments.of(new int[]{0}, 0),
                 Arguments.of(new int[]{1, 2, 3, 1}, 4),
-                Arguments.of(new int[]{2, 7, 9, 3, 1}, 12)
+                Arguments.of(new int[]{2, 7, 9, 3, 1}, 12),
+                Arguments.of(new int[]{2, 1, 1, 2}, 4),
+                Arguments.of(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 0)
         );
     }
 
@@ -29,4 +31,31 @@ class HouseRobberTest {
         // then
         Assertions.assertThat(robbed).isEqualTo(expected);
     }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testRobBottomUp(int[] nums, int expected) {
+        // given
+        HouseRobber solution = new HouseRobber();
+
+        // when
+        int robbed = solution.robBottomUp(nums);
+
+        // then
+        Assertions.assertThat(robbed).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testRobTopToBottom(int[] nums, int expected) {
+        // given
+        HouseRobber solution = new HouseRobber();
+
+        // when
+        int robbed = solution.robTopToBottom(nums);
+
+        // then
+        Assertions.assertThat(robbed).isEqualTo(expected);
+    }
+
 }
