a82e5b66d8cf73ba2be305f742aa2b076aeb597c
slemjet <slemjet@gmail.com>
2022-01-20 11:54:34 -0800

    875. Koko Eating Bananas

---
+++ questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/tasks_851_900/tasks/KokoEatingBananas.java
@@ -0,0 +1,48 @@
+package us.slemjet.leetcode.medium.tasks_851_900.tasks;
+
+import java.util.stream.IntStream;
+
+/**
+ * 875. Koko Eating Bananas
+ */
+public class KokoEatingBananas {
+
+    /**
+     * Use Binary Search
+     * min - 1, max = max(piles)
+     * get mid and check if with that ratio (mid) can eat all within h hours
+     * shift left or right to get closes to h and store min ratio value
+     *
+     * Runtime: 58.08%
+     * Memory Usage: 71.57%
+     */
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int left = 1;
+        int right = IntStream.of(piles).max().orElse(Integer.MIN_VALUE);
+        int result = right;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            // Calculate hours using current eat ratio (k)
+            int currHours = 0;
+            for (int pile : piles) {
+                currHours += pile / mid; // Hours to eat pile with current rate
+                if (pile % mid != 0) currHours++; // Need to eat the remainder also
+            }
+            if (currHours <= h) { // Acceptable condition - store min
+                result = Math.min(result, mid);
+            }
+            // Shift ranges accordingly
+            if (currHours > h) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+}
+
---
+++ questions_leetcode/medium/src/test/java/us/slemjet/leetcode/medium/tasks_851_900/tasks/KokoEatingBananasTest.java
@@ -0,0 +1,35 @@
+package us.slemjet.leetcode.medium.tasks_851_900.tasks;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class KokoEatingBananasTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(new int[]{3, 6, 7, 11}, 8, 4),
+                Arguments.of(new int[]{30, 11, 23, 4, 20}, 5, 30),
+                Arguments.of(new int[]{30, 11, 23, 4, 20}, 6, 23),
+                Arguments.of(new int[]{873375536,395271806,617254718,970525912,634754347,824202576,694181619,20191396,886462834,442389139,572655464,438946009,791566709,776244944,694340852,419438893,784015530,588954527,282060288,269101141,499386849,846936808,92389214,385055341,56742915,803341674,837907634,728867715,20958651,167651719,345626668,701905050,932332403,572486583,603363649,967330688,484233747,859566856,446838995,375409782,220949961,72860128,998899684,615754807,383344277,36322529,154308670,335291837,927055440,28020467,558059248,999492426,991026255,30205761,884639109,61689648,742973721,395173120,38459914,705636911,30019578,968014413,126489328,738983100,793184186,871576545,768870427,955396670,328003949,786890382,450361695,994581348,158169007,309034664,388541713,142633427,390169457,161995664,906356894,379954831,448138536},
+                        943223529, 46)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testMinEatingSpeed(int[] piles, int h, int expected) {
+        // given
+        KokoEatingBananas solution = new KokoEatingBananas();
+
+        // when
+        int result = solution.minEatingSpeed(piles, h);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+}
