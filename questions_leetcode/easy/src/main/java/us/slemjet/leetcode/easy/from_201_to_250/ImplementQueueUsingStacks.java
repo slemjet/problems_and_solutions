75c1bef7ab80dabf5677aeefdd16723fbdcd7400
slemjet <slemjet@gmail.com>
2022-01-20 17:37:16 -0800

    232. Implement Queue using Stacks

---
+++ questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/from_201_to_250/ImplementQueueUsingStacks.java
@@ -0,0 +1,91 @@
+package us.slemjet.leetcode.easy.from_201_to_250;
+
+import java.util.Stack;
+
+/**
+ * 232. Implement Queue using Stacks
+ */
+public class ImplementQueueUsingStacks {
+
+    /**
+     * Amortized version - re-order stacks only once per value - when output stack becomes empty
+     * <p>
+     * Runtime: 100.00%
+     * Memory Usage: 88.38%
+     */
+    static class MyQueueAmortized {
+
+        private final Stack<Integer> outStack; // Contains elements in reversed order - ready to be sent out
+        private final Stack<Integer> inStack; // Contains elements as they come in
+
+        public MyQueueAmortized() {
+            outStack = new Stack<>();
+            inStack = new Stack<>();
+        }
+
+        public void push(int x) {
+            inStack.push(x);
+        }
+
+        public int pop() {
+            if (outStack.isEmpty()) { // Need to move values from in to out stack
+                while (!inStack.empty()) {
+                    outStack.push(inStack.pop());
+                }
+            }
+            return outStack.pop();
+        }
+
+        public int peek() {
+            if (outStack.isEmpty()) { // Need to move values from in to out stack
+                while (!inStack.empty()) {
+                    outStack.push(inStack.pop());
+                }
+            }
+            return outStack.peek();
+        }
+
+        public boolean empty() {
+            return outStack.empty() && inStack.empty();
+        }
+    }
+
+    /**
+     * Basic version - re-order stacks on each input
+     * <p>
+     * Runtime: 100.00%
+     * Memory Usage: 8.83%
+     */
+    static class MyQueueBasic {
+
+        private final Stack<Integer> container;
+        private final Stack<Integer> temp;
+
+        public MyQueueBasic() {
+            container = new Stack<>();
+            temp = new Stack<>();
+        }
+
+        public void push(int x) {
+            while (!container.isEmpty()) {
+                temp.push(container.pop());
+            }
+            temp.push(x);
+            while (!temp.isEmpty()) {
+                container.push(temp.pop());
+            }
+        }
+
+        public int pop() {
+            return container.pop();
+        }
+
+        public int peek() {
+            return container.peek();
+        }
+
+        public boolean empty() {
+            return container.empty();
+        }
+    }
+}
---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/from_201_to_250/ImplementQueueUsingStacksTest.java
@@ -0,0 +1,59 @@
+package us.slemjet.leetcode.easy.from_201_to_250;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class ImplementQueueUsingStacksTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(new String[]{"MyQueue", "push", "push", "peek", "pop", "empty"},
+                        new Integer[]{null, 1, 2, null, null, null},
+                        new Object[]{null, null, null, 1, 1, false})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testImplementQueueUsingStacksAmortized(String[] commands, Integer[] params, Object[] expected) {
+        ImplementQueueUsingStacks.MyQueueAmortized myQueue = null;
+
+        for (int i = 0; i < commands.length; i++) {
+            if ("MyQueue".equals(commands[i]))
+                myQueue = new ImplementQueueUsingStacks.MyQueueAmortized();
+            else if ("push".equals(commands[i]))
+                myQueue.push(params[i]);
+            else if ("peek".equals(commands[i]))
+                Assertions.assertThat(myQueue.peek()).isEqualTo(expected[i]);
+            else if ("pop".equals(commands[i])) {
+                Assertions.assertThat(myQueue.pop()).isEqualTo(expected[i]);
+            } else if ("empty".equals(commands[i])) {
+                Assertions.assertThat(myQueue.empty()).isEqualTo(expected[i]);
+            }
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testImplementQueueUsingStacksBasic(String[] commands, Integer[] params, Object[] expected) {
+        ImplementQueueUsingStacks.MyQueueBasic myQueue = null;
+
+        for (int i = 0; i < commands.length; i++) {
+            if ("MyQueue".equals(commands[i]))
+                myQueue = new ImplementQueueUsingStacks.MyQueueBasic();
+            else if ("push".equals(commands[i]))
+                myQueue.push(params[i]);
+            else if ("peek".equals(commands[i]))
+                Assertions.assertThat(myQueue.peek()).isEqualTo(expected[i]);
+            else if ("pop".equals(commands[i])) {
+                Assertions.assertThat(myQueue.pop()).isEqualTo(expected[i]);
+            } else if ("empty".equals(commands[i])) {
+                Assertions.assertThat(myQueue.empty()).isEqualTo(expected[i]);
+            }
+        }
+    }
+}
