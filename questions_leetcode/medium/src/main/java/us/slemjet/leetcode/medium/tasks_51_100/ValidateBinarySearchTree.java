68a8268fe76073d58c61b4ab318f915258ce2357
slemjet <slemjet@gmail.com>
2022-01-23 13:19:22 -0800

    98. Validate Binary Search Tree

--- questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/tasks_51_100/ValidateBinarySearchTree.java
+++ questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/tasks_51_100/ValidateBinarySearchTree.java
@@ -8,7 +8,25 @@ import java.util.List;
  */
 public class ValidateBinarySearchTree {
 
+    /**
+     * Runtime: 50.41%
+     * Memory Usage: 28.86%
+     */
     public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    private boolean isValidBST(TreeNode root, Integer min, Integer max) {
+        if (root == null) return true;
+        if (min != null && root.val <= min || max != null && root.val >= max) return false;
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+
+    /**
+     * Runtime: 16.68%
+     * Memory Usage: 48.19%
+     */
+    public boolean isValidBST2(TreeNode root) {
         return traverseInorder(root, new ArrayList<>());
     }
 
--- questions_leetcode/medium/src/test/java/us/slemjet/leetcode/medium/tasks_51_100/ValidateBinarySearchTreeTest.java
+++ questions_leetcode/medium/src/test/java/us/slemjet/leetcode/medium/tasks_51_100/ValidateBinarySearchTreeTest.java
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import us.slemjet.leetcode.medium.tasks_51_100.ValidateBinarySearchTree;
 import us.slemjet.leetcode.medium.tasks_51_100.ValidateBinarySearchTree.TreeNode;
 
 import java.util.stream.Stream;
@@ -16,13 +15,15 @@ class ValidateBinarySearchTreeTest {
                 Arguments.of(new Integer[]{2, 1, 3}, true),
                 Arguments.of(new Integer[]{5, 1, 4, null, null, 3, 6}, false),
                 Arguments.of(new Integer[]{1, 1}, false),
-                Arguments.of(new Integer[]{10, 5, 15, null, null, 6, 20}, false)
+                Arguments.of(new Integer[]{10, 5, 15, null, null, 6, 20}, false),
+                Arguments.of(new Integer[]{0}, true),
+                Arguments.of(new Integer[]{2147483647}, true)
         );
     }
 
     @ParameterizedTest
     @MethodSource("parameters")
-    void testSolution(Integer[] input, boolean expected) {
+    void testIsValidBST(Integer[] input, boolean expected) {
         // given
         ValidateBinarySearchTree solution = new ValidateBinarySearchTree();
         TreeNode tree = new TreeNode(input[0]);
@@ -41,6 +42,32 @@ class ValidateBinarySearchTreeTest {
 
         // when
         Boolean isValidBST = solution.isValidBST(tree);
+
+        // then
+        Assertions.assertThat(isValidBST).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testIsValidBST2(Integer[] input, boolean expected) {
+        // given
+        ValidateBinarySearchTree solution = new ValidateBinarySearchTree();
+        TreeNode tree = new TreeNode(input[0]);
+        if (input.length > 1 && input[1] != null)
+            tree.left = new TreeNode(input[1]);
+        if (input.length > 2 && input[2] != null)
+            tree.right = new TreeNode(input[2]);
+        if (input.length > 3 && input[3] != null)
+            tree.left.left = new TreeNode(input[3]);
+        if (input.length > 4 && input[4] != null)
+            tree.left.right = new TreeNode(input[4]);
+        if (input.length > 5 && input[5] != null)
+            tree.right.left = new TreeNode(input[5]);
+        if (input.length > 6 && input[6] != null)
+            tree.right.right = new TreeNode(input[6]);
+
+        // when
+        Boolean isValidBST = solution.isValidBST2(tree);
 
         // then
         Assertions.assertThat(isValidBST).isEqualTo(expected);
