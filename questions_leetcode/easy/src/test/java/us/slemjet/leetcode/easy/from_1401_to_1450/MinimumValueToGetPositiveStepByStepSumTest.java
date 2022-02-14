f3e82d3ad2492c9612efb30ac4d4cb2a19f572e9
slemjet <slemjet@gmail.com>
2022-02-13 19:39:15 -0800

    1413. Minimum Value to Get Positive Step by Step Sum

---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/from_1401_to_1450/MinimumValueToGetPositiveStepByStepSumTest.java
@@ -0,0 +1,32 @@
+package us.slemjet.leetcode.easy.from_1401_to_1450;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class MinimumValueToGetPositiveStepByStepSumTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(new int[]{-3, 2, -3, 4, 2}, 5),
+                Arguments.of(new int[]{1, 2}, 1),
+                Arguments.of(new int[]{1, -2, -3}, 5)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testMinStartValue(int[] nums, int expected) {
+        // given
+        MinimumValueToGetPositiveStepByStepSum solution = new MinimumValueToGetPositiveStepByStepSum();
+
+        // when
+        int result = solution.minStartValue(nums);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
