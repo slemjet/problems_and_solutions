ebcc125a8873b0131e76fdef0b8b2fa39453308d
slemjet <slemjet@gmail.com>
2022-03-27 11:32:52 -0700

    1337. The K Weakest Rows in a Matrix

---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/from_1301_to_1350/tasks/TheKWeakestRowsInAMatrixTest.java
@@ -0,0 +1,59 @@
+package us.slemjet.leetcode.easy.from_1301_to_1350.tasks;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class TheKWeakestRowsInAMatrixTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(new int[][]{
+                        new int[]{1, 1, 0, 0, 0},
+                        new int[]{1, 1, 1, 1, 0},
+                        new int[]{1, 0, 0, 0, 0},
+                        new int[]{1, 1, 0, 0, 0},
+                        new int[]{1, 1, 1, 1, 1}}, 3, new int[]{2, 0, 3}),
+                Arguments.of(new int[][]{
+                        new int[]{1, 0, 0, 0},
+                        new int[]{1, 1, 1, 1},
+                        new int[]{1, 0, 0, 0},
+                        new int[]{1, 0, 0, 0}}, 2, new int[]{0, 2}),
+                Arguments.of(new int[][]{
+                        new int[]{1, 1, 1, 1, 1},
+                        new int[]{1, 0, 0, 0, 0},
+                        new int[]{1, 1, 0, 0, 0},
+                        new int[]{1, 1, 1, 1, 0},
+                        new int[]{1, 1, 1, 1, 1}}, 3, new int[]{1, 2, 3})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testKWeakestRows(int[][] mat, int k, int[] expected) {
+        // given
+        TheKWeakestRowsInAMatrix solution = new TheKWeakestRowsInAMatrix();
+
+        // when
+        int[] result = solution.kWeakestRows(mat, k);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testKWeakestRows2(int[][] mat, int k, int[] expected) {
+        // given
+        TheKWeakestRowsInAMatrix solution = new TheKWeakestRowsInAMatrix();
+
+        // when
+        int[] result = solution.kWeakestRows2(mat, k);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
