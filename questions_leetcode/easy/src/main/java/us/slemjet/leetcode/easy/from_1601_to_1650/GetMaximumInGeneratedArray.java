001f3f4a9ea30478a555b010a960c7cd505a85c7
slemjet <slemjet@gmail.com>
2022-11-06 12:36:36 -0800

    1646. Get Maximum in Generated Array

---
+++ questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/from_1601_to_1650/GetMaximumInGeneratedArray.java
@@ -0,0 +1,30 @@
+package us.slemjet.leetcode.easy.from_1601_to_1650;
+
+/**
+ * 1646. Get Maximum in Generated Array
+ */
+public class GetMaximumInGeneratedArray {
+
+    /**
+     * Runtime: 82.06%
+     * Memory Usage: 32.23%
+     */
+    public int getMaximumGenerated(int n) {
+
+        if (n < 2) return n;
+
+        int[] results = new int[n + 1];
+        results[1] = 1;
+
+        int max = 1;
+        for (int i = 2; i <= n; i++) {
+            results[i] = results[i / 2];
+            if (i % 2 == 1) {
+                results[i] += results[(i / 2) + 1];
+            }
+            max = Math.max(max, results[i]);
+        }
+
+        return max;
+    }
+}
