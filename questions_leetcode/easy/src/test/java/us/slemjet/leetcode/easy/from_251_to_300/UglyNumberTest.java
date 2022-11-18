89b53e06ddfde227eb2c6dc4b62f115468ae30fe
slemjet <slemjet@gmail.com>
2022-11-17 19:48:08 -0800

    263. Ugly Number

---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/from_251_to_300/UglyNumberTest.java
@@ -0,0 +1,34 @@
+package us.slemjet.leetcode.easy.from_251_to_300;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class UglyNumberTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(6, true),
+                Arguments.of(1, true),
+                Arguments.of(14, false),
+                Arguments.of(0, false),
+                Arguments.of(-2147483648, false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testIsUgly(int n, boolean expected) {
+        // given
+        UglyNumber solution = new UglyNumber();
+
+        // when
+        boolean result = solution.isUgly(n);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
