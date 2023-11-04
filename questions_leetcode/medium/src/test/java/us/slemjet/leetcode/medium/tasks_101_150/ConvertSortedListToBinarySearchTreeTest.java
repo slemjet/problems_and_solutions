6d92cfbc022515ec519b8f5f6fd17bbe36e535ab
slemjet <slemjet@gmail.com>
2023-11-03 21:36:05 -0700

    109. Convert Sorted List to Binary Search Tree - test fix

--- questions_leetcode/medium/src/test/java/us/slemjet/leetcode/medium/tasks_101_150/ConvertSortedListToBinarySearchTreeTest.java
+++ questions_leetcode/medium/src/test/java/us/slemjet/leetcode/medium/tasks_101_150/ConvertSortedListToBinarySearchTreeTest.java
@@ -11,7 +11,30 @@ import java.util.stream.Stream;
 
 class ConvertSortedListToBinarySearchTreeTest {
 
-    private static Stream<Arguments> parameters() {
+    private static Stream<Arguments> parameters2Pointers() {
+        return Stream.of(
+                Arguments.of(new ListNode(-10, new ListNode(-3, new ListNode(0, new ListNode(5, new ListNode(9))))),
+                        new TreeNode(0,
+                                new TreeNode(-3, new TreeNode(-10), null),
+                                new TreeNode(9, new TreeNode(5), null))),
+                Arguments.of(new ListNode(), new TreeNode())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters2Pointers")
+    void testSortedListToBST2Pointers(ListNode head, TreeNode expected) {
+        // given
+        ConvertSortedListToBinarySearchTree solution = new ConvertSortedListToBinarySearchTree();
+
+        // when
+        TreeNode result = solution.sortedListToBST2Pointers(head);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> parametersArray() {
         return Stream.of(
                 Arguments.of(new ListNode(-10, new ListNode(-3, new ListNode(0, new ListNode(5, new ListNode(9))))),
                         new TreeNode(0,
@@ -22,26 +45,13 @@ class ConvertSortedListToBinarySearchTreeTest {
     }
 
     @ParameterizedTest
-    @MethodSource("parameters")
+    @MethodSource("parametersArray")
     void testSortedListToBSTArray(ListNode head, TreeNode expected) {
         // given
         ConvertSortedListToBinarySearchTree solution = new ConvertSortedListToBinarySearchTree();
 
         // when
         TreeNode result = solution.sortedListToBSTArray(head);
-
-        // then
-        Assertions.assertThat(result).isEqualTo(expected);
-    }
-
-    @ParameterizedTest
-    @MethodSource("parameters")
-    void testSortedListToBST2Pointers(ListNode head, TreeNode expected) {
-        // given
-        ConvertSortedListToBinarySearchTree solution = new ConvertSortedListToBinarySearchTree();
-
-        // when
-        TreeNode result = solution.sortedListToBST2Pointers(head);
 
         // then
         Assertions.assertThat(result).isEqualTo(expected);
