09a40195f009a1b4bc114b281879c85f501cf3ea
slemjet <slemjet@gmail.com>
2023-12-06 21:42:27 -0800

    1903. Largest Odd Number in String

---
+++ questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/from_1901_to_1950/tasks/LargestOddNumberInString.java
@@ -0,0 +1,22 @@
+package us.slemjet.leetcode.easy.from_1901_to_1950.tasks;
+
+/**
+ * 1903. Largest Odd Number in String
+ */
+public class LargestOddNumberInString {
+
+    /**
+     * Time: O(1) -> 100.00%
+     * Space: O(1) -> 17.34%
+     */
+    public String largestOddNumber(String num) {
+
+        for (int i = num.length() - 1; i >= 0; i--) {
+            char charAt = num.charAt(i);
+            if (charAt == '1' || charAt == '3' || charAt == '5' || charAt == '7' || charAt == '9') {
+                return num.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
+}
---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/from_1901_to_1950/tasks/LargestOddNumberInStringTest.java
@@ -0,0 +1,32 @@
+package us.slemjet.leetcode.easy.from_1901_to_1950.tasks;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class LargestOddNumberInStringTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("52", "5"),
+                Arguments.of("4206", ""),
+                Arguments.of("35427", "35427")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testSolution(String num, String expected) {
+        // given
+        LargestOddNumberInString solution = new LargestOddNumberInString();
+
+        // when
+        String result = solution.largestOddNumber(num);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
