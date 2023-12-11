bb73501ce2a6e214c471dfef7b878b4268730843
slemjet <slemjet@gmail.com>
2023-12-10 16:36:54 -0800

    N-Queens II

---
+++ questions_leetcode/hard/src/test/java/us/slemjet/leetcode/hard/from_50_to_100/NQueensIITest.java
@@ -0,0 +1,31 @@
+package us.slemjet.leetcode.hard.from_50_to_100;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class NQueensIITest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(4, 2),
+                Arguments.of(1, 1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testSolution(int n, int expected) {
+        // given
+        NQueensII solution = new NQueensII();
+
+        // when
+        int result = solution.totalNQueens(n);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
