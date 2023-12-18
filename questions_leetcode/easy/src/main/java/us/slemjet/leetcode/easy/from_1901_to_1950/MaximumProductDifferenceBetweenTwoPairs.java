a591e7d545dc9738dee3c218794392c32b02aa7e
slemjet <slemjet@gmail.com>
2023-12-17 21:49:26 -0800

    1913. Maximum Product Difference Between Two Pairs

---
+++ questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/from_1901_to_1950/MaximumProductDifferenceBetweenTwoPairs.java
@@ -0,0 +1,38 @@
+package us.slemjet.leetcode.easy.from_1901_to_1950;
+
+import java.util.Arrays;
+
+/**
+ * 1913. Maximum Product Difference Between Two Pairs
+ */
+public class MaximumProductDifferenceBetweenTwoPairs {
+
+    /**
+     * Time: O(n) -> 80.07%
+     * Space: O(1) -> 6.62
+     */
+    public int maxProductDifference(int[] nums) {
+
+        int min = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE, max = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
+
+        for (int val : nums) {
+            if (min > val) {
+                min2 = min;
+                min = val;
+            } else {
+                min2 = Math.min(min2, val);
+            }
+
+            if (max < val) {
+                max2 = max;
+                max = val;
+            } else {
+                max2 = Math.max(max2, val);
+            }
+        }
+
+        int result = max * max2 - min * min2;
+
+        return result;
+    }
+}
---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/from_1901_to_1950/MaximumProductDifferenceBetweenTwoPairsTest.java
@@ -0,0 +1,31 @@
+package us.slemjet.leetcode.easy.from_1901_to_1950;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class MaximumProductDifferenceBetweenTwoPairsTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(new int[]{5, 6, 2, 7, 4}, 34),
+                Arguments.of(new int[]{4, 2, 5, 9, 7, 4, 8}, 64)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testSolution(int[] nums, int expected) {
+        // given
+        MaximumProductDifferenceBetweenTwoPairs solution = new MaximumProductDifferenceBetweenTwoPairs();
+
+        // when
+        int result = solution.maxProductDifference(nums);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
