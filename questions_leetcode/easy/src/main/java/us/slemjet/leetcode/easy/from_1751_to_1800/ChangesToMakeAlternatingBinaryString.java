b21b7874396f7f9a4206a3989f9e4e4c9f3cdcb5
slemjet <slemjet@gmail.com>
2023-12-23 22:14:39 -0800

    1758. Minimum Changes To Make Alternating Binary String

---
+++ questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/from_1751_to_1800/ChangesToMakeAlternatingBinaryString.java
@@ -0,0 +1,43 @@
+package us.slemjet.leetcode.easy.from_1751_to_1800;
+
+/**
+ * 1758. Minimum Changes To Make Alternating Binary String
+ */
+public class ChangesToMakeAlternatingBinaryString {
+
+    /**
+     * Time: O(n) -> 74.75%
+     * Space: O(1) -> 10.82%
+     */
+    public int minOperations(String s) {
+
+        int countStart0 = 0;
+        int countStart1 = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char currChar = s.charAt(i);
+            boolean isEvenIdx = i % 2 == 0;
+            boolean isZero = currChar == '0';
+
+            // Alternating starting from 0 > even numbers - 0, odd numbers = 1
+            // Alternating starting from 1 -> even numbers - 1, odd numbers = 0
+            if (isEvenIdx) {
+                // Even index -> Staring from 0 are expected to have 0, starting from 1 are expected to have 1
+                if (isZero) {
+                    countStart1++;
+                } else {
+                    countStart0++;
+                }
+            } else {
+                // Odd index -> Staring from 0 are expected to have 1, starting from 1 are expected to have 0
+                if (isZero) {
+                    countStart0++;
+                } else {
+                    countStart1++;
+                }
+            }
+        }
+
+        return Math.min(countStart0, countStart1);
+    }
+}
