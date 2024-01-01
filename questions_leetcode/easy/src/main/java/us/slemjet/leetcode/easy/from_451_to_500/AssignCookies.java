3b049511a4644b7f1c09d2646ef0a92565475185
slemjet <slemjet@gmail.com>
2023-12-31 21:11:07 -0800

    455. Assign Cookies

---
+++ questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/from_451_to_500/AssignCookies.java
@@ -0,0 +1,39 @@
+package us.slemjet.leetcode.easy.from_451_to_500;
+
+import java.util.Arrays;
+
+/**
+ * 455. Assign Cookies
+ */
+public class AssignCookies {
+
+    /**
+     * Time: O(nlogn)   ->  98.82%
+     * Space: O(1)      ->  61.93%
+     */
+    public int findContentChildren(int[] g, int[] s) {
+
+        int result = 0;
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int idx = 0;
+
+        for (int child : g) {
+
+            while (idx < s.length && s[idx] < child) {
+                // Try next cookie
+                idx++;
+            }
+
+            if (idx < s.length) {
+                // Can use cookie
+                result++;
+                idx++;
+            }
+        }
+
+        return result;
+    }
+}
