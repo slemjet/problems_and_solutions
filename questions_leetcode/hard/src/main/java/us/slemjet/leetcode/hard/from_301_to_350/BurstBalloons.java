951e91ee6e9eaa62a8387d480b82a00377d6f98e
slemjet <slemjet@gmail.com>
2024-01-02 15:06:28 -0800

    312. Burst Balloons

---
+++ questions_leetcode/hard/src/main/java/us/slemjet/leetcode/hard/from_301_to_350/BurstBalloons.java
@@ -0,0 +1,48 @@
+package us.slemjet.leetcode.hard.from_301_to_350;
+
+/**
+ * 312. Burst Balloons
+ */
+public class BurstBalloons {
+
+    /**
+     * Time: O(n^3) ->  5.15%
+     * Space:O(n^2) -> 5.22%
+     */
+    public int maxCoins(int[] nums) {
+
+        int length = nums.length;
+
+        // Create array, containing 1 at its boundaries
+        int[] withOnes = new int[length + 2];
+        System.arraycopy(nums, 0, withOnes, 1, length);
+        withOnes[0] = 1;
+        withOnes[withOnes.length - 1] = 1;
+
+        Integer[][] memo = new Integer[withOnes.length][withOnes.length];
+        return maxCoins(withOnes, 1, withOnes.length - 2, memo);
+    }
+
+    private int maxCoins(int[] nums, int left, int right, Integer[][] memo) {
+
+        if (left > right) {
+            return 0;
+        }
+
+        if (memo[left][right] != null) {
+            return memo[left][right];
+        }
+
+        int max = 0;
+
+        // For each position - get sum of left subset + right subset + outer boundaries product
+        for (int i = left; i <= right; i++) {
+            int leftCount = maxCoins(nums, left, i - 1, memo);
+            int rightCount = maxCoins(nums, i + 1, right, memo);
+            int currentCount = nums[left - 1] * nums[i] * nums[right + 1];
+            max = Math.max(max, leftCount + rightCount + currentCount);
+        }
+
+        return memo[left][right] = max;
+    }
+}
---
+++ questions_leetcode/hard/src/test/java/us/slemjet/leetcode/hard/from_301_to_350/BurstBalloonsTest.java
@@ -0,0 +1,31 @@
+package us.slemjet.leetcode.hard.from_301_to_350;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class BurstBalloonsTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(new int[]{3, 1, 5, 8}, 167),
+                Arguments.of(new int[]{1, 5}, 10)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testSolution(int[] nums, int expected) {
+        // given
+        BurstBalloons solution = new BurstBalloons();
+
+        // when
+        int result = solution.maxCoins(nums);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
