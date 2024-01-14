8c53037d8b231a2fd9cbf36f393c84859e2e44df
slemjet <slemjet@gmail.com>
2024-01-14 13:36:06 -0800

    357. Count Numbers with Unique Digits

--- questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/tasks_351_400/CountNumbersWithUniqueDigits.java
+++ questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/tasks_351_400/CountNumbersWithUniqueDigits.java
@@ -6,10 +6,69 @@ package us.slemjet.leetcode.medium.tasks_351_400;
 public class CountNumbersWithUniqueDigits {
 
     /**
-     *
+     * Time: O(1)   ->  100.00%
+     * Space:O(1)   ->
      */
     public int countNumbersWithUniqueDigits(int n) {
 
-        return 0;
+        // n = 0 -> 1
+        // n = 1 -> 10
+        // n = 2 -> 10 + 9 * 9
+        // n = 3 -> 10 + 9 * 9 * 8
+        // n = 4 -> 10 + 9 * 9 * 8 * 7
+
+        int[] dp = new int[9];
+        dp[0] = 1;
+        dp[1] = 10;
+
+        int product = 9;
+
+        for (int i = 2; i <= n; i++) {
+
+            product *= (11 - i);
+            dp[i] = dp[i - 1] + product;
+        }
+
+        return dp[n];
+    }
+
+    /**
+     * Time: O(?)   ->  6.59%
+     * Space:O(?)   ->  27.6
+     */
+    public int countNumbersWithUniqueDigitsBacktrack(int n) {
+
+        if (n > 10) {
+            // If there are more than 10 digits - it is not possible to create a number without duplicates
+            return countNumbersWithUniqueDigitsBacktrack(10);
+        }
+
+        boolean[] isDigitUsed = new boolean[10];
+
+        int maxNumber = (int) Math.pow(10, n);
+        return countCombinations(0, maxNumber, isDigitUsed);
+    }
+
+    private int countCombinations(int current, int maxNumber, boolean[] isDigitUsed) {
+
+        if (current >= maxNumber) {
+            return 0;
+        }
+
+        int count = 1;
+        // Start counting combinations
+
+        for (int i = 0; i <= 9; i++) {
+            if (current == 0 && i == 0) {
+                continue; // skip leading zeroes
+            }
+
+            if (!isDigitUsed[i]) {
+                isDigitUsed[i] = true; // Try all the combination if take this number
+                count += countCombinations(current * 10 + i, maxNumber, isDigitUsed);
+                isDigitUsed[i] = false;
+            }
+        }
+        return count;
     }
 }
--- questions_leetcode/medium/src/test/java/us/slemjet/leetcode/medium/tasks_351_400/CountNumbersWithUniqueDigitsTest.java
+++ questions_leetcode/medium/src/test/java/us/slemjet/leetcode/medium/tasks_351_400/CountNumbersWithUniqueDigitsTest.java
@@ -4,11 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import us.slemjet.leetcode.medium.tasks_451_500.PredictTheWinner;
 
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class CountNumbersWithUniqueDigitsTest {
 
@@ -16,7 +13,8 @@ class CountNumbersWithUniqueDigitsTest {
         return Stream.of(
                 Arguments.of(0, 1),
                 Arguments.of(1, 10),
-                Arguments.of(2, 91)
+                Arguments.of(2, 91),
+                Arguments.of(3, 739)
         );
     }
 
@@ -28,6 +26,18 @@ class CountNumbersWithUniqueDigitsTest {
 
         // when
         int result = solution.countNumbersWithUniqueDigits(n);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testSolutionTopDownBacktrack(int n, int expected) {
+        // given
+        CountNumbersWithUniqueDigits solution = new CountNumbersWithUniqueDigits();
+
+        // when
+        int result = solution.countNumbersWithUniqueDigitsBacktrack(n);
 
         // then
         Assertions.assertThat(result).isEqualTo(expected);
