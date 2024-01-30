7faaeab09a4c82dea5e3b54cc62bbb59873bdd34
slemjet <slemjet@gmail.com>
2024-01-29 21:17:12 -0800

    150. Evaluate Reverse Polish Notation

--- questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/tasks_101_150/EvaluateReversePolishNotation.java
+++ questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/tasks_101_150/EvaluateReversePolishNotation.java
@@ -8,36 +8,34 @@ import java.util.Stack;
 public class EvaluateReversePolishNotation {
 
     /**
-     * Runtime: 89.38%
-     * Memory Usage: 59.79%
+     * Time: O(n)   -> 78.23%
+     * Space:O(n)   -> 68.63%
      */
     public int evalRPN(String[] tokens) {
-        if (tokens == null || tokens.length == 0)
-            return 0;
 
+        if (tokens == null || tokens.length == 0) {
+            return 0;
+        }
+
+        // Store current operands to be processed
         Stack<Integer> operands = new Stack<>();
-        int idx = 0;
-        while (idx < tokens.length) {
-            String token = tokens[idx++];
-            if (!"+".equals(token) && !"-".equals(token) && !"*".equals(token) && !"/".equals(token)) {
+
+        for (String token : tokens) {
+            if (!("+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token))) {
+                // It's a number - just push to process later
                 operands.push(Integer.valueOf(token));
             } else {
+                // It's an operation sign - get 2 operands and compute operation
                 Integer operand2 = operands.pop();
                 Integer operand1 = operands.pop();
-                switch (token) {
-                    case "+":
-                        operands.push(operand1 + operand2);
-                        break;
-                    case "-":
-                        operands.push(operand1 - operand2);
-                        break;
-                    case "*":
-                        operands.push(operand1 * operand2);
-                        break;
-                    case "/":
-                        operands.push(operand1 / operand2);
-                        break;
-                }
+                Integer result = switch (token) {
+                    case "+" -> operand1 + operand2;
+                    case "-" -> operand1 - operand2;
+                    case "*" -> operand1 * operand2;
+                    case "/" -> operand1 / operand2;
+                    default -> 0;
+                };
+                operands.push(result);
             }
         }
 
