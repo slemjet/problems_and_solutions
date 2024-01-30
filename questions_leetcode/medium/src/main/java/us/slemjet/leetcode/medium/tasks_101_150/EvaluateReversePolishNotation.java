721dbeb9a8a06f26773759581b6090180f0c0120
slemjet <slemjet@gmail.com>
2024-01-29 21:34:05 -0800

    150. Evaluate Reverse Polish Notation

--- questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/tasks_101_150/EvaluateReversePolishNotation.java
+++ questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/tasks_101_150/EvaluateReversePolishNotation.java
@@ -1,5 +1,6 @@
 package us.slemjet.leetcode.medium.tasks_101_150;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 /**
@@ -8,7 +9,7 @@ import java.util.Stack;
 public class EvaluateReversePolishNotation {
 
     /**
-     * Time: O(n)   -> 78.23%
+     * Time: O(n)   -> 99.11%
      * Space:O(n)   -> 68.63%
      */
     public int evalRPN(String[] tokens) {
@@ -16,28 +17,38 @@ public class EvaluateReversePolishNotation {
         if (tokens == null || tokens.length == 0) {
             return 0;
         }
-
         // Store current operands to be processed
         Stack<Integer> operands = new Stack<>();
 
-        for (String token : tokens) {
-            if (!("+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token))) {
-                // It's a number - just push to process later
-                operands.push(Integer.valueOf(token));
-            } else {
-                // It's an operation sign - get 2 operands and compute operation
-                Integer operand2 = operands.pop();
-                Integer operand1 = operands.pop();
-                Integer result = switch (token) {
-                    case "+" -> operand1 + operand2;
-                    case "-" -> operand1 - operand2;
-                    case "*" -> operand1 * operand2;
-                    case "/" -> operand1 / operand2;
-                    default -> 0;
-                };
-                operands.push(result);
-            }
-        }
+        Arrays.stream(tokens).forEach(token -> {
+                    Integer result = switch (token) {
+                        case "+" -> {
+                            Integer operand2 = operands.pop();
+                            Integer operand1 = operands.pop();
+                            yield operand1 + operand2;
+                        }
+                        case "-" -> {
+                            Integer operand2 = operands.pop();
+                            Integer operand1 = operands.pop();
+                            yield operand1 - operand2;
+                        }
+                        case "*" -> {
+                            Integer operand2 = operands.pop();
+                            Integer operand1 = operands.pop();
+                            yield operand1 * operand2;
+                        }
+                        case "/" -> {
+                            Integer operand2 = operands.pop();
+                            Integer operand1 = operands.pop();
+                            yield operand1 / operand2;
+                        }
+                        // It's a number - just add for future processing
+                        default -> Integer.valueOf(token);
+                    };
+
+                    operands.push(result);
+                }
+        );
 
         return operands.pop();
     }
