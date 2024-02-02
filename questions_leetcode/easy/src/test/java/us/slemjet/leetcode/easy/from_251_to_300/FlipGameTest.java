589f1f6630f35234d8ef883a32516888c6cf5bd0
slemjet <slemjet@gmail.com>
2024-02-01 22:02:38 -0800

    293. Flip Game

---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/from_251_to_300/FlipGameTest.java
@@ -0,0 +1,32 @@
+package us.slemjet.leetcode.easy.from_251_to_300;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+class FlipGameTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("++++", List.of("--++", "+--+", "++--")),
+                Arguments.of("+", List.of())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testSolution(String currentState, List<String> expected) {
+        // given
+        FlipGame solution = new FlipGame();
+
+        // when
+        List<String> result = solution.generatePossibleNextMoves(currentState);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
