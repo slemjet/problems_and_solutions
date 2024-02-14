a5c83a2a550b81da116cc6813c44bb7f49f018d2
slemjet <slemjet@gmail.com>
2024-02-13 22:39:18 -0800

    2149. Rearrange Array Elements by Sign

---
+++ questions_leetcode/medium/src/main/java/us/slemjet/leetcode/medium/tasks_2101_2150/tasks/RearrangeArrayElementsBySign.java
@@ -0,0 +1,60 @@
+package us.slemjet.leetcode.medium.tasks_2101_2150.tasks;
+
+/**
+ * 2149. Rearrange Array Elements by Sign
+ */
+public class RearrangeArrayElementsBySign {
+
+    /**
+     * Time: O(n)   ->  100.00%
+     * Space: O(n)  ->  74.37%
+     */
+    public int[] rearrangeArray(int[] nums) {
+
+        int[] result = new int[nums.length];
+        int negIdx = 1; // Next negative (starts from idx 1)
+        int posIdx = 0;
+
+        for (int i = 0; i < result.length; i++) {
+
+            if (nums[i] > 0) {
+                result[posIdx] = nums[i];
+                posIdx += 2; // Set next positive idx
+            } else {
+                result[negIdx] = nums[i];
+                negIdx += 2; // Set next negative idx
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     *
+     */
+    public int[] rearrangeArray2(int[] nums) {
+
+        int[] result = new int[nums.length];
+        int negIdx = 0;
+        int posIdx = 0;
+
+        for (int i = 0; i < result.length; i++) {
+
+            if (i % 2 == 1) {
+                // get next negative
+                while (nums[negIdx] >= 0) {
+                    negIdx++;
+                }
+                result[i] = nums[negIdx++];
+            } else {
+                // get next positive
+                while (nums[posIdx] < 0) {
+                    posIdx++;
+                }
+                result[i] = nums[posIdx++];
+            }
+        }
+
+        return result;
+    }
+}
---
+++ questions_leetcode/medium/src/test/java/us/slemjet/leetcode/medium/tasks_2101_2150/tasks/RearrangeArrayElementsBySignTest.java
@@ -0,0 +1,44 @@
+package us.slemjet.leetcode.medium.tasks_2101_2150.tasks;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class RearrangeArrayElementsBySignTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of(new int[]{3, 1, -2, -5, 2, -4}, new int[]{3, -2, 1, -5, 2, -4}),
+                Arguments.of(new int[]{-1, 1}, new int[]{1, -1})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testSolution(int[] nums, int[] expected) {
+        // given
+        RearrangeArrayElementsBySign solution = new RearrangeArrayElementsBySign();
+
+        // when
+        int[] result = solution.rearrangeArray(nums);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testSolution2(int[] nums, int[] expected) {
+        // given
+        RearrangeArrayElementsBySign solution = new RearrangeArrayElementsBySign();
+
+        // when
+        int[] result = solution.rearrangeArray2(nums);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
