ec390a08662f3985b7150f0b794f8ad24d59f54f
Jet <slemjet@gmail.com>
2024-09-26 22:21:54 -0700

    844. Backspace String Compare

---
+++ questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/tasks_801_850/tasks/BackspaceStringCompare.java
@@ -0,0 +1,43 @@
+package us.slemjet.leetcode.easy.tasks_801_850.tasks;
+
+/**
+ * 844. Backspace String Compare
+ */
+public class BackspaceStringCompare {
+
+    /**
+     * Time: O(max(n,m))    ->  100%
+     * Space:O(1)           ->  40.5%
+     */
+    public boolean backspaceCompare(String s, String t) {
+        int sIdx = s.length() - 1;
+        int tIdx = t.length() - 1;
+
+        while (sIdx >= 0 || tIdx >= 0) {
+            sIdx = processBackspaces(s, sIdx);
+            tIdx = processBackspaces(t, tIdx);
+
+            if (sIdx < 0 || tIdx < 0) {
+                return sIdx < 0 && tIdx < 0; // Both must be less than 0
+            } else if (s.charAt(sIdx) != t.charAt(tIdx)) {
+                return false;
+            }
+            sIdx--;
+            tIdx--;
+        }
+        return true;
+    }
+
+    private int processBackspaces(String str, int index) {
+        int numHashes = 0;
+        while (index >= 0 && (str.charAt(index) == '#' || numHashes > 0)) {
+            if (str.charAt(index) == '#') {
+                numHashes++;
+            } else {
+                numHashes--;
+            }
+            index--;
+        }
+        return index;
+    }
+}
---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/tasks_801_850/tasks/BackspaceStringCompareTest.java
@@ -0,0 +1,41 @@
+package us.slemjet.leetcode.easy.tasks_801_850.tasks;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class BackspaceStringCompareTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("ab#c", "ad#c", true),
+                Arguments.of("ab##", "c#d#", true),
+                Arguments.of("a#c", "b", false),
+                Arguments.of("a##c", "#a#c", true),  // Both become "c"
+                Arguments.of("", "", true),  // Both are empty strings
+                Arguments.of("#", "", true),  // "#" results in an empty string
+                Arguments.of("a#b#c#", "", true),  // "a#b#c#" results in an empty string
+                Arguments.of("a#b#c#", "d#d#", true),  // Both result in an empty string
+                Arguments.of("###", "", true),  // Multiple backspaces on an empty string are still empty strings
+                Arguments.of("ab#c", "ad#c##", false),  // Different lengths after backspaces
+                Arguments.of("#a#bc#c", "bc", true),  // Both become "bc"
+                Arguments.of("bxj##tw", "bxo#j##tw", true)  // Both become "btw"
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testBackspaceCompare(String s, String t, boolean expected) {
+        // given
+        var solution = new BackspaceStringCompare();
+
+        // when
+        boolean result = solution.backspaceCompare(s, t);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
