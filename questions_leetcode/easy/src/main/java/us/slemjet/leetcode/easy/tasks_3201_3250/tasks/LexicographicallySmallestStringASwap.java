4169eb94738edba0984b97d285e95ebd16372bbc
Jet <slemjet@gmail.com>
2024-10-13 21:52:07 -0700

    3216. Lexicographically Smallest String After a Swap

---
+++ questions_leetcode/easy/src/main/java/us/slemjet/leetcode/easy/tasks_3201_3250/tasks/LexicographicallySmallestStringASwap.java
@@ -0,0 +1,30 @@
+package us.slemjet.leetcode.easy.tasks_3201_3250.tasks;
+
+/**
+ * 3216. Lexicographically Smallest String After a Swap
+ */
+public class LexicographicallySmallestStringASwap {
+
+    /**
+     * Time: O(n)   ->  100.00%
+     * Space:O(1)   ->  71.57%
+     */
+    public String getSmallestString(String s) {
+
+        char[] chars = s.toCharArray();
+
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] % 2 != chars[i - 1] % 2) continue;
+
+            if (chars[i] < chars[i - 1]) {
+                // Swap
+                char tmp = chars[i];
+                chars[i] = chars[i - 1];
+                chars[i - 1] = tmp;
+                break;
+            }
+        }
+
+        return new String(chars);
+    }
+}
---
+++ questions_leetcode/easy/src/test/java/us/slemjet/leetcode/easy/tasks_3201_3250/tasks/LexicographicallySmallestStringASwapTest.java
@@ -0,0 +1,31 @@
+package us.slemjet.leetcode.easy.tasks_3201_3250.tasks;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class LexicographicallySmallestStringASwapTest {
+
+    private static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.of("45320", "43520"),
+                Arguments.of("001", "001")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void testSolution(String s, String expected) {
+        // given
+        LexicographicallySmallestStringASwap solution = new LexicographicallySmallestStringASwap();
+
+        // when
+        String result = solution.getSmallestString(s);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
